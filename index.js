c80094e286e311b5c4593d3ead7dac9659c375c7
Scott Corgan <scottcorgan@gmail.com>
2014-01-06 10:46:21 -0800

    Return to slice.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var splice = [].splice;
+var slice = [].slice;
 var ctx = document;
 
 module.exports = function (selector, multiple) {
   return (typeof selector == 'string')
-    ? (multiple) ? splice.call(ctx.querySelectorAll(selector), 0) : ctx.querySelector(selector)
-    : (selector.length) ? splice.call(selector, 0) : selector;
+    ? (multiple) ? slice.call(ctx.querySelectorAll(selector), 0) : ctx.querySelector(selector)
+    : (selector.length) ? slice.call(selector, 0) : selector;
 };
