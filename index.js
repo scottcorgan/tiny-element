31cf9a82eb9e7296dfa67f09fa8675177345bde2
Dmitry <df.creative@gmail.com>
2014-09-15 21:31:21 +0400

    Use function context as a query context

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var slice = [].slice;
-var ctx = document;
 
 module.exports = function (selector, multiple) {
+  var ctx = this === window ? document : this;
+
   return (typeof selector == 'string')
     ? (multiple) ? slice.call(ctx.querySelectorAll(selector), 0) : ctx.querySelector(selector)
     : (selector.length) ? slice.call(selector, 0) : selector;
